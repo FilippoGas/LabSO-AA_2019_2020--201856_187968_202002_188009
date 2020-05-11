53a015b6a33708c4fe3f1197521533c8d453bb4d
EttoreEroe <favar.ttore@gmail.com>
2020-05-11 19:09:11 +0200

    Modifica processoP

--- src/Pprocess/processPfunc.c
+++ src/Pprocess/processPfunc.c
@@ -19,25 +19,28 @@ int readInput(int argc, char *argv[], int *m, int *pipe_read, int *pipe_write, c
 	(*files) = arrayStringSubset(argc, 4, argv);
 	return argc - 4;
 }
-
-char ***createQmatrix(int m, int pipe_write, char **files, int nfiles){
+/*
+char ***createQmatrix(int m, int pipe[2], char **files, int nfiles){
 	char ***res = (char ***)malloc(m * sizeof(char **));
 	int i = 0;
 	while(i < m){
 		res[i] = (char **)calloc((5 + nfiles), sizeof(char *));
-		
+
 		res[i][0] = (char *)malloc((strlen(QNAME) + 1) * sizeof(char));
 		sprintf(res[i][0], "%s", QNAME);
-		
+
 		res[i][1] = (char *)malloc((INTMAXCHAR + 1) * sizeof(char));
-		sprintf(res[i][1], "%d", pipe_write);
-		
+		sprintf(res[i][4], "%d", pipe[READ]);
+
+		res[i][1] = (char *)malloc((INTMAXCHAR + 1) * sizeof(char));
+		sprintf(res[i][3], "%d", pipe[WRITE]);
+
 		res[i][2] = (char *)malloc((INTMAXCHAR + 1) * sizeof(char));
 		sprintf(res[i][2], "%d", m);
-		
+
 		res[i][3] = (char *)malloc((INTMAXCHAR + 1) * sizeof(char));
-		sprintf(res[i][3], "%d", i);
-		
+		sprintf(res[i][1], "%d", i);
+
 		int j = 0;
 		while(j < nfiles){
 			res[i][j + 4] = (char *)malloc((strlen(files[j]) + 1) * sizeof(char));
@@ -48,6 +51,59 @@ char ***createQmatrix(int m, int pipe_write, char **files, int nfiles){
 	}
 	return res;
 }
+*/
+
+void createChildren(char **argvQ){
+        //Creo Q
+        int f=fork();
+        while(f<0){
+                f=fork();
+        }
+        if(f==0){
+        //exec order 66
+        //execpv(argvQ[0],argvQ);
+        }
+}
+
+
+long lungnum(int m){
+        int i=1;
+        while(m/10!=0){
+        m=m/10;
+        i++;
+        }
+        return i;
+}
+
+
+char **create_ArgvQ(int m, int pipe[2], char **files, int nfiles){
+        char **ret;
+        ret[0]=(char *)malloc(strlen(QNAME)+1);
+        sprintf(ret[0], "%s", QNAME);
+
+        ret[1]=(char *)malloc((INTMAXCHAR+1) * sizeof(char));
+
+        ret[2]=(char *)malloc(lungnum(m)+1);
+        sprintf(ret[2], "%d", m);
+
+        ret[3]=(char *)malloc(lungnum(pipe[READ])+1);
+        sprintf(ret[3], "%d", pipe[READ]);
+
+        ret[4]=(char *)malloc(lungnum(pipe[WRITE])+1);
+        sprintf(ret[4], "%d", pipe[WRITE]);
+        int i=0;
+        while(i<nfiles){
+                ret[i+5]=(char *)malloc(strlen(files[i])+1);
+                sprintf(ret[i+5], "%s", files[i]);
+        i++;
+        }
+        ret[i+5]=NULL;
+        return ret;
+}
+
+
+
+
 
 void freeStringArray(char **in, int dim){
 	int i = 0;
@@ -70,4 +126,16 @@ void freeArgsForQ(char *** matrix, int row){
 		row--;
 	}
 	free(matrix);
+}
+
+
+int exitMessage(char *mess){
+	int ret=0;
+	if(strlen(mess)>5)
+		ret=1;
+	return ret;
+}
+
+int notNULL(char *mess){
+	return !strcmp(ENDM, mess);
 }
