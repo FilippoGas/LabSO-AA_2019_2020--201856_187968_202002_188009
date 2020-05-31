b2ded796aa3568d253a799a63486d2463d9e833d
beppe-99 <lamorgese.giuseppe99@gmail.com>
2020-05-31 02:13:21 +0200

    Aggiunta gestione di aggiunta/rimozione file in Q (parte da testare ancora)

--- src/Qprocess/processQ.c
+++ src/Qprocess/processQ.c
@@ -1,33 +1,48 @@
 #include "processQfunc.h"
+
 int main(int argc, char *argv[]){
-  int parte, denominatore, pipeRead, pipeWrite;
-  int *writtenFile = (int *)calloc(argc-ARGS_Q_START_FILE_OFFSET,sizeof(int));
-  readInput(argc, argv, &parte, &denominatore, &pipeRead, &pipeWrite);
+  int parte, denominatore, pipeRead, pipeWrite, pipeReadOnTheFly, pipeWriteOnTheFly;
+  int *writtenFiles = (int *)calloc(argc-ARGS_Q_START_FILE_OFFSET,sizeof(int));
+  int *removedFiles = (int *)calloc(argc-ARGS_Q_START_FILE_OFFSET,sizeof(int));
+  char message[PIPE_BUF+1];
+  int counterFilesOnTheFly = argc-1;
+  readInput(argc, argv, &parte, &denominatore, &pipeRead, &pipeWrite, &pipeReadOnTheFly, &pipeWriteOnTheFly);
   close(pipeRead);
 
   //Salva i file descriptor in un array
   int i=0;
   while(i<argc - ARGS_Q_START_FILE_OFFSET){
-    	//CONTROLLER SULLA PIPE DEI MESSAGGI DA P
-	int fd  = openFile(argv[i + ARGS_Q_START_FILE_OFFSET]);
+    //CONTROLLER SULLA PIPE DEI MESSAGGI DA P
+    int byteRead = read(pipeReadOnTheFly,message,PIPE_BUF);
+    if(byteRead!=0){
+      if(!strcmp(message,MOD_ADD)){
+        addHandler(pipeReadOnTheFly,pipeWrite,counterFilesOnTheFly,parte,denominatore,message);
+      }
+      else if(!strcmp(message,MOD_REMOVE)){
+        removeHandler(pipeReadOnTheFly,removedFiles,writtenFiles,message,argc,argv);
+      }
+    }
+
+	  int fd  = openFile(argv[i + ARGS_Q_START_FILE_OFFSET]);
     int size = computeSize(fd);
-  //Per ogni file vado a calcolarmi l'offset, l'end e la stringa formato che andrò a scrivere nella pipe
+    //Per ogni file vado a calcolarmi l'offset, l'end e la stringa formato che andrò a scrivere nella pipe
     int offset = computeOffset(parte,denominatore,size);
     int end = computeEnd(parte,denominatore,size);
     char *format = computeCountingOnFile(fd,i + ARGS_P_START_FILE_OFFSET,offset,end);	//mi serve indice
     //printf("Scrivo nella pipe: %s\n", format);
     //Scrivo nella pipe la stringa formato
     if(errorSysCall(write(pipeWrite,format, PIPE_BUF))>-1){
-      writtenFile[i-ARGS_P_START_FILE_OFFSET] = 1;
+      writtenFiles[i-ARGS_P_START_FILE_OFFSET] = 1;
     }
     //Stampo a video la stringa formato
     //printFormatString(format);
-   errorSysCall(close(fd));
+    errorSysCall(close(fd));
     free(format);
     i++;
   }
   errorSysCall(close(pipeWrite));
-  free(writtenFile);
+  free(writtenFiles);
+  free(removedFiles);
 	//FREE INPUT DATA
   return 0;
 }
--- src/Qprocess/processQfunc.h
+++ src/Qprocess/processQfunc.h
@@ -15,6 +15,12 @@ int computeSize(int fileDescriptor);
 
 int openFile(char *name);
 
-void readInput(int argc, char *argv[], int *parte, int *denominatore, int *pipeRead, int *pipeWrite);
+void readInput(int argc, char *argv[], int *parte, int *denominatore, int *pipeRead, int *pipeWrite, int *pipeReadOnTheFly, int *pipeWriteOnTheFly);
 
-int isWrittenFile(int *writtenFile,int idFile);
+int isWrittenFile(int *writtenFiles,int idFile);
+
+void addHandler(int pipeReadOnTheFly, int pipeWrite, int counterFilesOnTheFly, int parte, int denominatore, char *message);
+
+void removeHandler(int pipeReadOnTheFly, int *removedFiles, int *writtenFiles, char *message, int argc, char *argv[]);
+
+int idFile(char *file, int argc, char *argv[]);
