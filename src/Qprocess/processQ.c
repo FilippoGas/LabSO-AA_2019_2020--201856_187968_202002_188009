a4f2772aeed4ea7e4ef08663b0d6d1e0a05894b1
beppe-99 <lamorgese.giuseppe99@gmail.com>
2020-06-03 17:55:10 +0200

    Aggiunto l'append di argv + altre modifiche nella gestione di aggiunta/rimozione file

--- src/Qprocess/processQ.c
+++ src/Qprocess/processQ.c
@@ -5,10 +5,9 @@ int main(int argc, char *argv[]){
   int *writtenFiles = (int *)calloc(argc-ARGS_Q_START_FILE_OFFSET,sizeof(int));
   int *removedFiles = (int *)calloc(argc-ARGS_Q_START_FILE_OFFSET,sizeof(int));
   char message[PIPE_BUF+1];
-  int counterFilesOnTheFly = argc-1;
   readInput(argc, argv, &parte, &denominatore, &pipeRead, &pipeWrite, &pipeReadOnTheFly, &pipeWriteOnTheFly);
   close(pipeRead);
-
+  
   //Salva i file descriptor in un array
   int i=0;
   while(i<argc - ARGS_Q_START_FILE_OFFSET){
@@ -16,30 +15,34 @@ int main(int argc, char *argv[]){
     int byteRead = read(pipeReadOnTheFly,message,PIPE_BUF);
     if(byteRead>0){
       if(!strcmp(message,MOD_ADD)){
-        addHandler(pipeReadOnTheFly,pipeWrite,counterFilesOnTheFly,parte,denominatore,message);
+        addHandler(pipeReadOnTheFly,pipeWrite,parte,denominatore,message,&argc,argv);
+        writtenFiles = realloc(writtenFiles,argc*sizeof(int));
+        removedFiles = realloc(removedFiles,argc*sizeof(int));
       }
       else if(!strcmp(message,MOD_REMOVE)){
         removeHandler(pipeReadOnTheFly,removedFiles,writtenFiles,message,argc,argv);
       }
     }
 
-	  int fd  = openFile(argv[i + ARGS_Q_START_FILE_OFFSET]);
-    int size = computeSize(fd);
-    //Per ogni file vado a calcolarmi l'offset, l'end e la stringa formato che andrò a scrivere nella pipe
-    int offset = computeOffset(parte,denominatore,size);
-    int end = computeEnd(parte,denominatore,size);
-    char *format = computeCountingOnFile(fd,i + ARGS_P_START_FILE_OFFSET,offset,end);	//mi serve indice
-    //printf("Scrivo nella pipe: %s\n", format);
-    //Scrivo nella pipe la stringa formato
     if(!removedFiles[i]){
+  	  int fd  = openFile(argv[i + ARGS_Q_START_FILE_OFFSET]);
+      int size = computeSize(fd);
+      //Per ogni file vado a calcolarmi l'offset, l'end e la stringa formato che andrò a scrivere nella pipe
+      int offset = computeOffset(parte,denominatore,size);
+      int end = computeEnd(parte,denominatore,size);
+      char *format = computeCountingOnFile(fd,i + ARGS_P_START_FILE_OFFSET,offset,end);	//mi serve indice
+      //printf("Scrivo nella pipe: %s\n", format);
+      //Scrivo nella pipe la stringa formato
+      //if(!removedFiles[i]){
       if(errorSysCall(write(pipeWrite,format, PIPE_BUF))>-1){
         writtenFiles[i] = 1;
       }
+      errorSysCall(close(fd));
+      free(format);
     }
     //Stampo a video la stringa formato
     //printFormatString(format);
-    errorSysCall(close(fd));
-    free(format);
+
     i++;
   }
   errorSysCall(close(pipeWrite));
